6d9be230ad1ef33d1c111d998373e0b930439db4
arjund-06 <davearjun25@gmail.com>
2023-01-10 20:28:16 +0530

    Day1 and Day2

---
+++ app.js
@@ -0,0 +1,69 @@
+console.log("Hi I am the file");
+
+let a = 10;
+var b = "String";
+const c = 10.5;
+let bool = true;
+
+// a += 2
+sum = a + c;
+tri = a + b;
+/*
+-=
+*=
+/=
+
+
+Arithmetic Ops
+
++ Additon
+- Subtraction
+* Multiplication
+/ Division
+** Exponent
+% Modulus
+
+Relational Opts
+
+>
+<
+>=
+<=
+==
+===
+
+*/
+
+
+
+let arr = [22, 5, 5, 8, 4, 2, "THis is a string", ["THis is another array", true, false, 10.5]];
+//          0  1  2  3  4  5  6                    7 
+
+// console.log(arr[1])
+// console.log(arr[5])
+// console.log(arr[4])
+// console.log(arr[6])
+
+let value = "Im a variable";
+let json = {
+    name: "Arjun",
+    age: 21,
+    j: {
+        key: value
+    }
+};
+
+if (12 == 122) {
+
+    let c = true
+
+    console.log(c);
+} else if (1 == '1') {
+
+    console.log("IF ELSE");
+
+} else {
+
+    console.log("ELSE");
+}
+console.log("After If");
